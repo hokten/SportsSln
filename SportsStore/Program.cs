a0d428b03dfa834ad96c47b1cd7b871bd9e41f12
hokten <hhoohh2001@yahoo.com>
2024-05-16 13:47:14 +0300

    MVC özelliği eklendi.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); // MVC kabiliyeti kazandırıyor.
+
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();  //Homecontroller index methodu icin
 
 app.Run();
