853160f6d3352dbe76e2e903f8be4b1a8875223f
hokten <hhoohh2001@yahoo.com>
2024-05-23 14:42:05 +0300

    Veritabanı işlemleri

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
 namespace SportsStore.Controllers {
 	public class HomeController: Controller {
 		private readonly StoreDbContext _context;
