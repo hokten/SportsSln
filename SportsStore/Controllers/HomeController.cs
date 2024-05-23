98847147a1809a07b4ad9fdabcd1e55c8f74ede8
hokten <hhoohh2001@yahoo.com>
2024-05-23 14:25:11 +0300

    Controllera StoreDbContext eklendi.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,8 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 namespace SportsStore.Controllers {
 	public class HomeController: Controller {
+		private readonly StoreDbContext _context;
+		
+		public HomeController(StoreDbContext context)
+		{
+			_context = context;
+		}		
+		
 		public IActionResult Index() {
 			return View();
 		}
 	}
 }
+
