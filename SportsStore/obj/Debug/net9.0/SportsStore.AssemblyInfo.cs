3f89fb1046fb0596c5ad5eea1fdff55901c52664
hokten <hhoohh2001@yahoo.com>
2025-04-15 16:27:24 +0300

    producteklendi migrationu eklendi.
    dotnet ef migrations add producteklendi

--- SportsStore/obj/Debug/net9.0/SportsStore.AssemblyInfo.cs
+++ SportsStore/obj/Debug/net9.0/SportsStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecab28b18456df96b3cc431a9ff826e1d07dfe91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87365a3113ab332dacdf01d949cfac24e03f707e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
